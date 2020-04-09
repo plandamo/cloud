76bec8b37163617f043724c6463ad4f3d95917fa
liujinshanaini <1075695320@qq.com>
2020-04-09 15:51:02 +0800

    优化返回结果类

--- cloud-commons/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
+++ cloud-commons/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
@@ -22,4 +22,8 @@ public class CommonResult<T> {
     private String errorMassage;
     private T data;
 
+    public CommonResult(int errorCode, String errorMassage) {
+        errorCode = this.errorCode;
+        errorMassage = this.errorMassage;
+    }
 }
